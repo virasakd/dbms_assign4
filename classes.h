58a18f7c7b01700884ecedacccda45001986754f
Desmond V-Holmes <virasakd@oregonstate.edu>
2023-02-25 20:46:07 -0800

    added functionality for inserting a record into a bucket

--- classes.h
+++ classes.h
@@ -4,7 +4,89 @@
 #include <iostream>
 #include <sstream>
 #include <bitset>
+#include <cmath>
 using namespace std;
+
+struct Header {
+    uint8_t numSlots;
+    uint8_t slots[5];
+    uint8_t nextBlock;
+};
+
+
+// create and return a header struct
+Header createHeader(char* buffer) {
+    Header header = {}; // zero-initialize the struct
+
+    header.numSlots = static_cast<uint8_t>(buffer[0]);
+    for (int i = 0; i < 5; i++) {
+        header.slots[i] = static_cast<uint8_t>(buffer[i + 1]);
+    }
+
+    header.nextBlock = static_cast<uint8_t>(buffer[6]);
+    return header;
+}
+
+
+// conver the struct header back to a char array.
+char* return_header(Header head){
+
+    char* updated_header = new char[8];
+    memset(updated_header,'\0',sizeof(updated_header));
+    updated_header[0] =(int)head.numSlots;
+
+    for (int i = 0; i< 5;i++){
+        updated_header[i+1] =(int)head.slots[i];
+    };
+
+    updated_header[6] = (int)head.nextBlock;
+    return updated_header;
+
+}
+
+// compute the has and return the i last bits
+int return_last_i_bits(int record_id, int i_bits, int num_buckets){
+    double temp = pow(2,4); 
+    int result = temp;
+
+    // compute the hash function.
+    int hash_outcome = record_id  % result;
+    int get_i_mask = (1 << i_bits) - 1; // create a mask of i bits set to 1
+    // grab the last i bits from the hash outcome
+    int last_ibits = hash_outcome & get_i_mask; // apply the mask to the number
+
+
+    // check if the last i bits >= number of bucktets in the directory.
+    if (last_ibits >= num_buckets){
+        cout << "flip is needed." << endl;
+            int flip_mask = 1 << (i_bits-1);
+
+        // Flip the ith bit using XOR
+        last_ibits = last_ibits ^ flip_mask;
+    }
+    return last_ibits;
+
+}
+
+
+// convert and int to string to char array.
+
+char* return_id(int id){
+    char* id_array = new char[9];
+    string temp_string = to_string( id);
+
+    strcpy(id_array, temp_string.c_str());
+
+    for(int i = 0; i < 8;i++){
+        cout << id_array[i];
+    }
+    cout << endl;
+
+    return id_array;
+
+}
+
+
 
 class Record {
 public:
@@ -31,7 +113,8 @@ class LinearHashIndex {
 
 private:
     const int BLOCK_SIZE = 4096;
-    const int HEADER_SIZE = 12;
+    const int HEADER_SIZE = 7;
+    const int RECORD_SIZE = 716;
 
     vector<int> blockDirectory; // Map the least-significant-bits of h(id) to a bucket location in EmployeeIndex (e.g., the jth bucket)
                                 // can scan to correct bucket using j*BLOCK_SIZE as offset (using seek function)
@@ -39,85 +122,208 @@ private:
     int n;  // The number of indexes in blockDirectory currently being used
     int i;	// The number of least-significant-bits of h(id) to check. Will need to increase i once n > 2^i
     int numRecords;    // Records currently in index. Used to test whether to increase n
+    int blockRecords;   // number of records in non overflow blocks
     int nextFreeBlock; // Next place to write a bucket. Should increment it by BLOCK_SIZE whenever a bucket is written to EmployeeIndex
+    int total_buckets;  // total buckets in the Index file
     int block_size;
     string fName = "";
     fstream outfile;
+    
+    // write a dynamic char array to the file.
+    void dynamic_to_file(int offset_spot, char *content_arary){
+        outfile.seekp(offset_spot,ios::beg); 
+        outfile.write(content_arary,sizeof(content_arary));
 
-    // Insert new record into index
+    }
+
+    // write the record to the open spot
+    void record_open_spot(Record record, int start_position){
+
+        char * id_array = return_id(record.id);
+        char* manager = return_id(record.manager_id);
+
+       
+        char name[201];
+        memset(name,'\0',sizeof(name));
+        char bio[501];
+        memset(bio,'\0', sizeof(bio));
+        strcpy(name, record.name.c_str());
+        strcpy(bio, record.bio.c_str());
+
+        // write record to the file.
+        //write the id, which is 8 bytes.
+        int offset_tracker = start_position;
+        dynamic_to_file(offset_tracker,id_array);
+
+        //write the manager id, which is also 8 bytes
+        offset_tracker += 8;
+        dynamic_to_file(offset_tracker, manager);
+        offset_tracker += 8;
+
+        // write the name, which is 200 bytes
+        outfile.seekp(offset_tracker,ios::beg); 
+        outfile.write(name,sizeof(name));
+
+        offset_tracker += 200;
+        // finally write the bio, which 500 bytes
+        outfile.seekp(offset_tracker,ios::beg); 
+        outfile.write(bio,sizeof(bio));
+       
+        // clear alloacted memory
+        delete[] id_array;
+        delete[] manager;
+
+
+    }
+
+    // start the process for inserting a record into a bucket
+    void insert_into_bucket(Record record, Header header,int block_index){
+
+        header.numSlots += 1;
+        int open_index = 0;
+
+        // find the first open slot in the page header.
+        for(int i = 0; i < 5; i++){
+            if((int)header.slots[i] == 0){
+                header.slots[i] = 1;
+                open_index = i;
+                break;
+
+            }
+            
+        }
+        cout << "id: " << record.id << " bucket -> " << block_index << " slot: " << open_index << endl;
+
+        // write updated header back to the file.
+        char *updated_header = return_header(header);
+        dynamic_to_file(block_index * BLOCK_SIZE, updated_header);
+        delete[] updated_header;
+
+        //find the position of the open slot
+        int insert_offset = (RECORD_SIZE * open_index) + ((block_index * BLOCK_SIZE) + HEADER_SIZE);
+
+        // cout << insert_offset << ": initial offset position." << endl;
+        record_open_spot(record,insert_offset);
+       
+    }
+   
+    // Insert new record into file
     void insertRecord(Record record) {
 
-        // No records written to index yet
-        outfile.open(fName, ios::out | ios::app);
+        // create the first 4 buckets
         if (numRecords == 0) {
+            outfile.open(fName, ios::out | ios::app);
             // Initialize index with first blocks (start with 4)
-            blockDirectory.push_back(0);
-            blockDirectory.push_back(1);
-            blockDirectory.push_back(2);
-            blockDirectory.push_back(3);
 
-            // initialize the header for the first blocks.
-            char emptyBuffer[4084];
-            memset(emptyBuffer, '0', sizeof(emptyBuffer));
-            // set the file point to the first page, and write the string bitmap.
-            outfile.seekp(0*BLOCK_SIZE,ios::beg);
-            outfile << "-,-,-,-,-," << "0" << ":";
-            outfile.write(emptyBuffer,sizeof(emptyBuffer));
+            char header[7];
+            memset(header,0,sizeof(header));
+            char emptyBuffer[4089];
+            memset(emptyBuffer, 45, sizeof(emptyBuffer));
 
-            // set the file pointer to the second page, and write the string bitmap.
-            outfile.seekp(1*BLOCK_SIZE,ios::beg);
-            outfile << "+,+,+,+,+," << "0" << ":";
-            outfile.write(emptyBuffer,sizeof(emptyBuffer));
+            // create the first four buckets and their headers.
+            for(int i = 0; i < 4; i++){
+                // add the bucket index tot the block directory
+                blockDirectory.push_back(i);
+                
+                // set the file pointer to the start of the bucket
+                outfile.seekp(i*BLOCK_SIZE,ios::beg);
+                // write the header, then the rest of the space in the bucket. 7 bytes for the header, 4089 for the records
+                outfile.write(header,sizeof(header));
+                outfile.write(emptyBuffer,sizeof(emptyBuffer));
 
-            // set the file pointer to the second page, and write the string bitmap.
-            outfile.seekp(2*BLOCK_SIZE,ios::beg);
-            outfile << "?,?,?,?,?," << "0" << ":";
-            outfile.write(emptyBuffer,sizeof(emptyBuffer));
+            }
+            // set the offset to the next open bucket space.
+            nextFreeBlock = 4;
 
-            // set the file pointer to the second page, and write the string bitmap.
-            outfile.seekp(3*BLOCK_SIZE,ios::beg);
-            outfile << "<,<,<,<,<,"<< 0 << ":";
-            outfile.write(emptyBuffer,sizeof(emptyBuffer));
-
-            nextFreeBlock = BLOCK_SIZE * 4;
-
-            
+            total_buckets = 4;
+            outfile.close();
 
         }
-         outfile.close();
-
-
-        // first check if the 
-        // first check if the _bit maps match?_
         
-        // these are the number of bits we want to compare
-        int bit_mask = (1 << i) - 1;
+        outfile.open(fName, ios::binary | ios::in | ios::out);
 
-        if ((record.id & bit_mask) >= (n & bit_mask)) {
-            cout << "last two bits are equal " <<  bit_mask << " " << record.id << endl;
-        }else{
-            cout << "last are not equal " << bit_mask << " " << record.id  << endl;
-        }
+        // compute the hash function and return the last i bits.
+        int last_ibits = return_last_i_bits(record.id,i,n);
+        int block_index = blockDirectory[last_ibits];
 
         
-        outfile.open(fName, ios::in);
+        // loop until we find an open space to insert the record.
+        bool inserting = true;
+        bool overflow_flag = false;
+        while(inserting){
 
-        for(int i = 0; i < 4;i++){
-            outfile.seekg(i*BLOCK_SIZE,ios::beg);
-            string line;
-            getline(outfile, line,':');
-            cout << line << endl;
+            // grab the header of the current block
+            char headerTxt[7];
+            outfile.seekg(block_index * BLOCK_SIZE,ios::beg);
+            outfile.read(headerTxt,HEADER_SIZE);
+            Header bucket_header = createHeader(headerTxt);
+            
+            // check if there are blocks open in this bucket.
+
+            if ((int)bucket_header.numSlots < 5){
+                // if so, find the first open spot in the bucket.
+
+                // write the block header first.
+                insert_into_bucket(record,bucket_header,block_index);
+                inserting = false;
+
+                
+                // only cout records that have been inserted into non overflow blocks.
+                if(!overflow_flag){
+                    blockRecords++;
+                }
+                
+
+            }// if there aren't any spots open, check for a over flow bucket. If one exist then iterate to it, otherwise create the overflow bucket.
+            else{
+                // need to modularize this later
+                if((int)bucket_header.nextBlock == 0){
+
+                    // update the overflow pointer in the header of the current bucket
+                    bucket_header.nextBlock = nextFreeBlock;
+
+                    cout << record.name << " overflow person " << endl;
+
+                    // write the updated header back to the current bucket
+                    char *new_header = return_header(bucket_header);
+                    dynamic_to_file(block_index * BLOCK_SIZE,new_header);
+                    delete[] new_header;
+
+                    // set the block index to the next overflow bucket
+                    block_index = nextFreeBlock;
+                    
+                    // cout << "next spot: " << block_index << " offset bytes = " << block_index * BLOCK_SIZE << endl;
+
+                    outfile.seekp(block_index *BLOCK_SIZE,ios::beg);
+
+                    // write the new overflow bucket to the file.
+                    char header[7];
+                    memset(header,0,sizeof(header));
+                    char emptyBuffer[4089];
+                    memset(emptyBuffer, 45, sizeof(emptyBuffer));
+                    outfile.write(header,sizeof(header));
+                    outfile.write(emptyBuffer,sizeof(emptyBuffer));
+                    nextFreeBlock++;
+                    total_buckets++;
+
+                    //set the overvlow flag.
+                    overflow_flag = true;
+                    
+
+                }else{
+                    // move the current page index to the over flow 
+                    block_index = (int)bucket_header.nextBlock;
+                }
+
+            }
+
 
             
-
         }
+
+        // outfile.flush();
         outfile.close();
 
-        // Add record to the index in the correct block, creating a overflow block if necessary
-
-
-        // Take neccessary steps if capacity is reached:
-		// increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
 
 
     }
@@ -129,9 +335,10 @@ public:
         numRecords = 0;
         block_size = 0;
         fName = indexFileName;
-
-        
+        blockRecords = 0;
+        total_buckets = 0;
     }
+
 
     // Read csv file and add records to the index
     void createFromFile(string csvFName) {
@@ -139,8 +346,7 @@ public:
         // open the input file.
         fstream input_file;
         input_file.open(csvFName, ios::in);
-
-
+        int check = 0;
 
         // begin reading the contents of the file.
         bool flag = true;
@@ -175,7 +381,13 @@ public:
                 cout << "end of the file"  << line << endl;
                 flag = false;
             }
-            break;
+            
+            if(check >= 22){
+                break;
+            }
+            check++;
+            
+            
 
         }
         input_file.close();
--- main.cpp
+++ main.cpp
@@ -18,33 +18,35 @@ using namespace std;
 
 int main(int argc, char* const argv[]) {
 
-    // Create the index
+    //Create the index
     LinearHashIndex emp_index("EmployeeIndex");
     emp_index.createFromFile("Employee.csv");
-    
+
+  
+
     // Loop to lookup IDs until user is ready to quit
-    while (1) {
-        string choice;
-        int id;
-        printf("Please enter either desired ID or 'quit' to end program.\n");
-        cin >> choice;
-        if (choice == "quit") {
-            break;
-        }
-        id = stoi(choice);
-        Record desired_record = emp_index.findRecordById(id);
-        // check if desired_record has null value for ID (-1)
-        //      print error if so
-        if (desired_record.id == -1) {
-            printf("ID: %d NOT FOUND\n", id);
-            // desired_record.print();
-        }
-        else {
-            desired_record.print();
-        }
-        // printf("desired_record.id: %d \n", desired_record.id);
-        // findRecordById(id);
-    }
+    // while (1) {
+    //     string choice;
+    //     int id;
+    //     printf("Please enter either desired ID or 'quit' to end program.\n");
+    //     cin >> choice;
+    //     if (choice == "quit") {
+    //         break;
+    //     }
+    //     id = stoi(choice);
+    //     Record desired_record = emp_index.findRecordById(id);
+    //     // check if desired_record has null value for ID (-1)
+    //     //      print error if so
+    //     if (desired_record.id == -1) {
+    //         printf("ID: %d NOT FOUND\n", id);
+    //         // desired_record.print();
+    //     }
+    //     else {
+    //         desired_record.print();
+    //     }
+    //     // printf("desired_record.id: %d \n", desired_record.id);
+    //     // findRecordById(id);
+    // }
     
 
     return 0;
